cd43fc7e4c513857a9dca5f42c6f3fdbbfd206c0
Ariesta <odikk60@gmail.com>
2019-02-16 21:06:12 +0800

    Update DB

--- toko.sql
+++ toko.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 04, 2019 at 04:57 AM
+-- Generation Time: Feb 16, 2019 at 02:05 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.3.1
 
@@ -32,16 +32,39 @@ CREATE TABLE `laptop` (
   `id` int(11) UNSIGNED NOT NULL,
   `merk_laptop` varchar(100) NOT NULL,
   `tipe_laptop` varchar(100) NOT NULL,
-  `ready` int(5) NOT NULL
+  `ready` int(5) NOT NULL,
+  `harga` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `laptop`
 --
 
-INSERT INTO `laptop` (`id`, `merk_laptop`, `tipe_laptop`, `ready`) VALUES
-(1, 'HP', 'CE086TX', 32),
-(2, 'Asus', 'GL553VM', 20);
+INSERT INTO `laptop` (`id`, `merk_laptop`, `tipe_laptop`, `ready`, `harga`) VALUES
+(1, 'HP', 'CE086TX', 32, 0),
+(2, 'Asus', 'GL553VM', 20, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `name`) VALUES
+(1, 'dadang', '123', 'dadang'),
+(6, 'odikk', '123', 'Ariesta'),
+(7, 'odikk', 'odikk', 'odikk');
 
 --
 -- Indexes for dumped tables
@@ -54,6 +77,12 @@ ALTER TABLE `laptop`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -61,7 +90,13 @@ ALTER TABLE `laptop`
 -- AUTO_INCREMENT for table `laptop`
 --
 ALTER TABLE `laptop`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
