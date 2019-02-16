6cab0580b1ca6478407e50f4a4e6f5be511e9e60
Ariesta <odikk60@gmail.com>
2019-02-16 21:44:13 +0800

    Final Search Function

--- src/tech/devatacreative/ListTableLaptop.java
+++ src/tech/devatacreative/ListTableLaptop.java
@@ -17,13 +17,29 @@ public class ListTableLaptop extends MainInventory {
      JTable tblTableListLaptop;
      JButton btnSearch;
      JScrollPane jspTblScroll;
+     private JRadioButton rbHarga;
+     ButtonGroup rbGroup = new ButtonGroup();
+
+    public void setRbGroup() {
+        rbGroup.add(rbMerk);
+        rbGroup.add(rbTipe);
+        rbGroup.add(rbReady);
+        rbGroup.add(rbHarga);
+        rbMerk.setSelected(true);
+    }
 
     public ListTableLaptop(){
-       model = new DefaultTableModel();
-       model.addColumn("Merk Laptop");
-       model.addColumn("Tipe Laptop");
-       model.addColumn("Jumlah Ready");
-       model.addColumn("Harga");
+        setRbGroup();
+        model = new DefaultTableModel(){
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
+        model.addColumn("Merk Laptop");
+        model.addColumn("Tipe Laptop");
+        model.addColumn("Jumlah Ready");
+        model.addColumn("Harga");
         String sql = "SELECT * FROM laptop";
         try {
             makeConnection();
@@ -51,9 +67,72 @@ public class ListTableLaptop extends MainInventory {
 
     private void searchLaptop(){
         String search;
-        rbMerk
+
         if (rbMerk.isSelected()){
             String sql = "SELECT * FROM laptop WHERE merk_laptop=?";
+            search = tfSearch.getText();
+            try {
+                makeConnection();
+                preparedStatement = connection.prepareStatement(sql);
+                preparedStatement.setString(1, search);
+                result = preparedStatement.executeQuery();
+                model.setRowCount(0);
+                while (result.next()){
+                    String a = result.getString(2);
+                    String b = result.getString(3);
+                    Integer c = result.getInt(4);
+                    Integer d = result.getInt(5);
+                    model.addRow(new Object[]{a,b,c,d});
+                }
+                tblTableListLaptop.setModel(model);
+//            tblTableListLaptop;
+            }catch (SQLException e){
+                JOptionPane.showMessageDialog(null, e.getMessage());
+            }
+        } else if (rbTipe.isSelected()){
+            String sql = "SELECT * FROM laptop WHERE tipe_laptop=?";
+            search = tfSearch.getText();
+            try {
+                makeConnection();
+                preparedStatement = connection.prepareStatement(sql);
+                preparedStatement.setString(1, search);
+                result = preparedStatement.executeQuery();
+                model.setRowCount(0);
+                while (result.next()){
+                    String a = result.getString(2);
+                    String b = result.getString(3);
+                    Integer c = result.getInt(4);
+                    Integer d = result.getInt(5);
+                    model.addRow(new Object[]{a,b,c,d});
+                }
+                tblTableListLaptop.setModel(model);
+//            tblTableListLaptop;
+            }catch (SQLException e){
+                JOptionPane.showMessageDialog(null, e.getMessage());
+            }
+        } else if (rbReady.isSelected()){
+            String sql = "SELECT * FROM laptop WHERE ready=?";
+            search = tfSearch.getText();
+            try {
+                makeConnection();
+                preparedStatement = connection.prepareStatement(sql);
+                preparedStatement.setString(1, search);
+                result = preparedStatement.executeQuery();
+                model.setRowCount(0);
+                while (result.next()){
+                    String a = result.getString(2);
+                    String b = result.getString(3);
+                    Integer c = result.getInt(4);
+                    Integer d = result.getInt(5);
+                    model.addRow(new Object[]{a,b,c,d});
+                }
+                tblTableListLaptop.setModel(model);
+//            tblTableListLaptop;
+            }catch (SQLException e){
+                JOptionPane.showMessageDialog(null, e.getMessage());
+            }
+        } else if (rbHarga.isSelected()){
+            String sql = "SELECT * FROM laptop WHERE harga=?";
             search = tfSearch.getText();
             try {
                 makeConnection();
